0ecd7335d5f249ffb2e860b84004095e7693e624
Joe DeSoto <modulusman@gmail.com>
2017-10-05 18:49:35 -0700

    fixes Pages controller

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,2 +1,5 @@
-<h2><?= $title ?></h2>
+<h2><?= 'This is an echo line of PHP code as an h2' ?></h2>
 <p>This is Joe's blog, version 1.0</p>
+<p>What worked for me was: <code>localhost:8080/php_blog/pages/view/about</code></p>
+<p>What did NOT for me was: <code>localhost:8080/php_blog/pages/view/about.php</code></p>
+<p>I'm now able to edit this page and see the parsed PHP code</p>
