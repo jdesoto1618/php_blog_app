b5dc7c692d58542cdd12978f10cc59c7ca0d5930
Joe DeSoto <modulusman@gmail.com>
2017-10-08 11:57:51 -0700

    enables text truncating feature, to 50 words per post. clicking view post shows the full text pf the post

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -6,7 +6,7 @@
   <!-- post title. also get the post date and display it next to the title -->
   <h3><?= $post['title']?> <small class="post_date">Posted on <em><?= $post['created_at']?></em></small></h3>
   <!-- post body -->
-  <p><?= $post['body']?></p>
+  <p><?= word_limiter($post['body'], 50)?></p>
   <br>
   <!-- link to view a specific post. use site_url so the address becomes /php_blog/posts/slugname -->
   <p><a class="btn btn-primary view_post" href="<?= site_url('/posts/'.$post['slug']); ?>">View Post</a></p>
