91a42008d1a1ff94360e329695e87e3a8fac92c0
Joe DeSoto <modulusman@gmail.com>
2017-10-26 19:50:21 -0700

    adds two sections on landing page, one image and one text. also adds custom cursor on hover state for landing page image

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,3 +1,12 @@
 <!-- the title is from the array built in the Pages controller. It contains the page name, as it appears in the address bar. This is what will be rendered when using $title -->
-<h1 class="text-center">Welcome to Live Journal!</h1>
-<img  class="bg_image" src="http://thecutestblogontheblock.com/wp-content/uploads/2012/06/Love-In-Literature-3C-free-blog-background-layout-template-vintage-valentine-1000x500.jpg">
+<h1 class="text-center home_page_title">Welcome to Live Journal!</h1>
+<hr class="horizontal">
+<div class="row">
+  <div class="col-xs-6">
+    <img  class="bg_image" src="http://www.theallium.com/wp-content/uploads/2014/05/LowImpactJournal.jpg">
+  </div>
+  <div class="col-xs-6">
+    <h3 class="text-center home_section_title">Share Your Thoughts on Live Journal</h3>
+    <p class=" text-center home_section_body">Blogs are still alive and well, as people in the Internet age find themselves with easy access to myriad ways of voicing opinions. This is one such outlet, where you can create post categories, and then make blog posts in that category, comment on other users posts, and upload pictures with your posts. Other standard features to registered users are the ability to delete post categories you created (though this still needs work, as it would delete all posts in that category, even from other users), delete your own posts, and of course edit them. The site is still being updated, and ideas are welcome! Thank you for visiting.</p>
+  </div>
+</div>
